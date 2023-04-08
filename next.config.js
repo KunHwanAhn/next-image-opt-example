f633d82c2373e2bcb26bee1f1bfd8f7c215dac8b
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-04-08 15:53:28 +0900

    next.js + TypeScript + ES Lint 환경 설정 (#1)
    * next.js + TypeScript + ES Lint 환경 설정
    
    * Readme 수정

---
+++ next.config.js
@@ -0,0 +1,32 @@
+/** @type {import('next').NextConfig} */
+const defaultConfig = {
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    domains: [
+      'conan-dev.xyz',
+      'image.oliveyoung.co.kr',
+    ],
+  },
+};
+
+/** @type {import('next').NextConfig} */
+const devConfig = {
+  ...defaultConfig,
+};
+
+/** @type {import('next').NextConfig} */
+const prodConfig = {
+  ...defaultConfig,
+  compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
+};
+
+/** @type {import('next').NextConfig} */
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
+module.exports = config;
