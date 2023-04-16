7fa61fb6f2c2ebda168f714711d9761f7e810230
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-04-16 17:12:35 +0900

    이미지 종류에 따른 예제(jpg, webp, avif) (#3)

---
+++ src/pages/image-types/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import { calcHeight } from '@/utils';
+
+import PageTitle from '@/components/PageTitle';
+import ImageCard from '@/components/ImageCard';
+
+export default function ImageTypes() {
+  const width = 1200;
+  const images = ['/bg-1.jpg', '/bg-1.webp', '/bg-1.avif'];
+
+  return (
+    <>
+      <Head>
+        <title>Image types</title>
+      </Head>
+      <PageTitle title="Image Types (jpg, webp, avif)" />
+
+      {images.map((src) => (
+        <ImageCard
+          key={`src-${src}`}
+          title={`src: ${src}`}
+        >
+          <Image
+            src={src}
+            alt="sample 1"
+            width={`${width}px`}
+            height={`${calcHeight(width)}px`}
+            quality={100}
+          />
+        </ImageCard>
+      ))}
+    </>
+  );
+}
