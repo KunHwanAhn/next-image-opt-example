26c2c01280e1694c122be96cb6cfebc55c13c51c
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-04-16 17:45:01 +0900

    이미지 유형 - svg 케이스 추가

--- src/pages/image-types/index.tsx
+++ src/pages/image-types/index.tsx
@@ -9,7 +9,7 @@ import ImageCard from '@/components/ImageCard';
 
 export default function ImageTypes() {
   const width = 1200;
-  const images = ['/bg-1.jpg', '/bg-1.webp', '/bg-1.avif'];
+  const images = ['https://conan-dev.xyz/img/logo/react.svg', '/bg-1.jpg', '/bg-1.webp', '/bg-1.avif'];
 
   return (
     <>
