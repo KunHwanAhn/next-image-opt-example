528afaf0d3c10015b029bc2205392232311a1711
Conan Kunhwan Ahn <kunhwon@gmail.com>
2023-04-20 00:45:55 +0900

    이미지 유형 추가 (e.g. svg) (#4)
    * 이미지 유형 - svg 케이스 추가
    
    * 제목 수정

--- src/pages/image-types/index.tsx
+++ src/pages/image-types/index.tsx
@@ -9,14 +9,14 @@ import ImageCard from '@/components/ImageCard';
 
 export default function ImageTypes() {
   const width = 1200;
-  const images = ['/bg-1.jpg', '/bg-1.webp', '/bg-1.avif'];
+  const images = ['https://conan-dev.xyz/img/logo/react.svg', '/bg-1.jpg', '/bg-1.webp', '/bg-1.avif'];
 
   return (
     <>
       <Head>
         <title>Image types</title>
       </Head>
-      <PageTitle title="Image Types (jpg, webp, avif)" />
+      <PageTitle title="Image Types (svg, jpg, webp, avif)" />
 
       {images.map((src) => (
         <ImageCard
